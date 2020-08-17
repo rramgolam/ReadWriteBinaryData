2daecace6a5bb22f0a9d3eae314eb4bac974c8a9
Rishi Ramgolam <33223925+rramgolam@users.noreply.github.com>
2020-08-17 14:27:37 +0100

    Initial commit

---
+++ src/com/company/readwritebinary/Main.java
@@ -0,0 +1,73 @@
+package com.company.readwritebinary;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Inner class for a Person
+        class Person {
+            public String name;
+            public int age;
+            public char sex;
+
+            public Person(String name, int age, char sex) {
+                this.name = name;
+                this.age = age;
+                this.sex = sex;
+            }
+        }
+
+	    // Writing Data
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("data.dat")))) {
+
+            locFile.writeUTF("Hi Jeff");
+            locFile.writeInt(79);
+            locFile.writeChar('M');
+
+            locFile.writeUTF("Hi Billie");
+            locFile.writeInt(60);
+            locFile.writeChar('F');
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Reading Data
+        String name = null;
+        int age = 0;
+        char sex = 0;
+        List<Person> people = new ArrayList<>();
+
+
+        try (DataInputStream locFile = new DataInputStream(new BufferedInputStream(new FileInputStream("data.dat")))) {
+
+            boolean eof = false;
+            try {
+                while(!eof) {
+                    name = locFile.readUTF();
+                    age = locFile.readInt();
+                    sex = locFile.readChar();
+                    people.add(new Person(name,age,sex));
+                }
+            } catch (EOFException e) {
+                eof = true;
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Person person : people) {
+            System.out.println("Name : " + person.name + " age: " + person.age + " sex: " + person.sex);
+
+        }
+
+    }
+}
